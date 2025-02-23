554da3f37022635cca79a9b977b3f9837233aa60
denis_stepanok <elfdenis@gmail.com>
2025-02-23 10:54:39 +0200

    F

--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,10 @@
 import axios from 'axios';
 
 const API_KEY = 'uHSLi07StIOlriMPxJGxUbSYsHDs6AFx';
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
+
+async function fetchData(page) {
+  const response = await axios.get(`${BASE_URL}events.json?apikey=${API_KEY}&`);
+}
+
+// https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey={apikey}
